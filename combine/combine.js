44fa78c4800a61dda78f6bb9d5c19e5a61ad3de6
Sean Brewer <seabre986@gmail.com>
2014-09-03 10:52:36 -0400

    Fix UTF8 issue with index.json output. Rework combine to use mostly streams during processing.

--- combine/combine.js
+++ combine/combine.js
@@ -1,6 +1,8 @@
 var request = require('request'),
   _ = require('lodash'),
-  Q = require('q'); 
+  Q = require('q'),
+  JSONStream = require('JSONStream'),
+  es = require('event-stream');
 
 function getDatasetCount(baseURL) {
   var deferred = Q.defer();
@@ -19,31 +21,43 @@ function numberOfPages(datasetCount, resultsPerPage) {
   return Math.ceil(datasetCount / resultsPerPage);
 }
 
-function combineRequest(baseURL, page) {
-  var deferred = Q.defer();
-  request({url: baseURL + '/data.json?page=' + page, json: true}, function (error, response, body) {
-    if (error) {
-      deferred.reject(new Error(error));
-    } else {
-      deferred.resolve(body);
-    }
-  });
-
-  return deferred.promise;
+function combineCatalogStreams(baseURL, pages, page, stream) {
+  if (pages == page) {
+    return es.merge(
+      stream,
+      request({url: baseURL + '/data.json?page=' + page})
+      .pipe(JSONStream.parse('*')));
+  } else {
+    return combineCatalogStreams(
+      baseURL,
+      pages,
+      page + 1,
+      es.merge(
+        stream,
+        request({url: baseURL + '/data.json?page=' + page})
+        .pipe(JSONStream.parse('*')))
+      );
+  }
 }
 
-function combineRequests(baseURL, pages) {
-  return Q.all(_.map(_.range(1, pages + 1), function(i) {
-    return combineRequest(baseURL, i);
-  })).then(function(j) {
-    return _.flatten(j, true);
-  });
+function getCatalogStream(baseURL, pages) {
+  if (pages == 1) {
+    return request({url: baseURL + '/data.json?page=1'})
+      .pipe(JSONStream.parse('*'))
+  } else {
+    return combineCatalogStreams(
+      baseURL,
+      pages,
+      2,
+      request({url: baseURL + '/data.json?page=1'})
+      .pipe(JSONStream.parse('*')));
+  }
 }
 
 function combineJSONCatalog(baseURL, resultsPerPage) {
   var deferred = Q.defer();
   getDatasetCount(baseURL).then(function (count) {
-    deferred.resolve(combineRequests(baseURL, numberOfPages(count, resultsPerPage)));
+    deferred.resolve(getCatalogStream(baseURL, numberOfPages(count, resultsPerPage)));
   });
 
   return deferred.promise;
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 var express = require('express'),
   combine = require('../combine/combine'),
-  es = require('event-stream'),
   JSONStream = require('JSONStream'),
   router = express.Router();
 
 router.get('/:baseScheme/:baseDomain/data.json', function(req, res) {
   combine.combineJSONCatalog(req.params.baseScheme + '://' + req.params.baseDomain, 100).then(function(result) {
-    res.setHeader("content-type", "application/json");
-    es.readArray(result)
+    res.setHeader("content-type", "application/json; charset=utf-8");
+    result
       .pipe(JSONStream.stringify())
       .pipe(res);
   });
